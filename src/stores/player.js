c1151417708e7ee92e45c1df9d83c489db20dd43
jbiddulph <john.mbiddulph@gmail.com>
2022-08-13 01:15:11 +0100

    users can pause / play songs

--- src/stores/player.js
+++ src/stores/player.js
@@ -2,21 +2,39 @@ import { defineStore } from "pinia";
 import { Howl } from "howler";
 
 export default defineStore("player", {
-    state: () => ({
-        current_song: {},
-        sound: {}
-    }),
-    actions: {
-        async newSong(song) {
-            this.current_song = song;
+  state: () => ({
+    current_song: {},
+    sound: {},
+  }),
+  actions: {
+    async newSong(song) {
+      this.current_song = song;
 
-            this.sound = new Howl({
-                src: [song.url],
-                html5: true,
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true,
+      });
 
-            });
+      this.sound.play();
+    },
+    async toggleAudio() {
+      if (!this.sound.playing) {
+        return;
+      }
 
-            this.sound.play();
-        }
-    }
-})
+      if (this.sound.playing()) {
+        this.sound.pause();
+      } else {
+        this.sound.play();
+      }
+    },
+  },
+  getters: {
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
+  },
+});
